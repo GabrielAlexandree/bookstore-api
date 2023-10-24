ca3ef5d8e8ec6f9a6fe23efab4c1401e115d3db4
GabrielAlexandree <dev.gabrielalexandre@gmail.com>
2023-10-24 16:39:42 -0300

    listando todos os livros por categoria

--- src/main/java/com/gabriel/bookstore/service/LivroService.java
+++ src/main/java/com/gabriel/bookstore/service/LivroService.java
@@ -1,5 +1,6 @@
 package com.gabriel.bookstore.service;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,10 +15,18 @@ public class LivroService {
 	
 	@Autowired
 	private LivroRepository repository;//faz a comunicacao com a base de dados  vai iniciar mostrar e destruir essa instancia 
+	
+	@Autowired
+	private CategoriaService categoriaService;
 
 	public Livro findById(Integer id) {
 		Optional<Livro> obj = repository.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não Encontrado! ID : "  + id + ", Tipo: " + Livro.class.getName()));
+	}
+
+	public List<Livro> findAll(Integer id_cat) {
+		categoriaService.findById(id_cat);
+		return repository.findAllByCategoria(id_cat);
 	}
 	
 }
